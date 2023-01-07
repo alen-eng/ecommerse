a8e84aa96f2a6f056cb0e5d99912e14382cc70d9
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-01-07 12:50:43 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ const state ={
 }
 module.exports.connect=function(done){
     //const Password=process.env.tvZwiltv8ylWVSkA
-   const url= process.env.MONGO_URL;
+   //const url= process.env.MONGO_URL;
+    const url= 'mongodb+srv://Admin-007:tvZwiItv8yIWVSkA@cluster-007.kxziv.mongodb.net/Cluster-007';
     //const url='mongodb://localhost:27017'
     const dbname='shopping'
 
